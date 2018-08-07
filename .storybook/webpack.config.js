5c0282491600a1f73513e27b5b39142c0d9a81b6
VINOD CHANDRA <vinodchandra.net@gmail.com>
2018-08-07 00:36:26 -0400

    integerate autoprefixer with storybook and remove sass-loader from the webpack

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,13 +1,43 @@
 const path = require('path')
+const autoprefixer = require('autoprefixer')
 
 module.exports = {
   module: {
     rules: [
       {
         test: /\.css$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [
+          require.resolve('style-loader'),
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: require.resolve('postcss-loader'),
+            options: {
+              // Necessary for external CSS imports to work
+              // https://github.com/facebookincubator/create-react-app/issues/2677
+              ident: 'postcss',
+              plugins: () => [
+                require('postcss-flexbugs-fixes'),
+                autoprefixer({
+                  browsers: [
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 9', // React doesn't support IE8 anyway
+                  ],
+                  flexbox: 'no-2009',
+                }),
+              ],
+            },
+          },
+        ],
         include: path.resolve(__dirname, '../src'),
       },
     ],
   },
+  plugins: [require('autoprefixer')],
 }
