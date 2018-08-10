9401df2aedda514b9effdd34ed01cdf524bee5e8
VINOD CHANDRA <vinodchandra.net@gmail.com>
2018-08-09 22:54:11 -0400

    updated routes/home component

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,50 @@
 import React, { Component, Fragment } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
 import { connect } from 'react-redux'
-import SecuredRoute from './SecuredRoute'
-import CssBaseline from '@material-ui/core/CssBaseline'
+import Home from './Home'
 
 class App extends Component {
   render() {
+    const { loggedin } = this.props
     return (
       <Router>
         <Fragment>
-          <CssBaseline />
-          <Switch>
-            <Route path="/login" render={() => <div>Login</div>} />
-            <SecuredRoute path="/" exact render={() => <div>Home</div>} />
-            <SecuredRoute
-              path="/question/create"
-              render={() => <div>Create New Question</div>}
-            />
-            <SecuredRoute
-              exact
-              path="/question/:id"
-              render={() => <div>Submit Question Answer</div>}
-            />
-            <SecuredRoute
-              exact
-              path="/question/poll/:id"
-              render={() => <div>Question Poll View</div>}
-            />
-            <SecuredRoute
-              path="/leaderboard"
-              render={() => <div>Leader Board</div>}
-            />
-            <SecuredRoute render={() => <h2>404 page not found</h2>} />
-          </Switch>
+          {loggedin ? (
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route
+                exact
+                path="/question/create"
+                render={() => <div>Create New Question</div>}
+              />
+              <Route
+                exact
+                path="/question/:id"
+                render={() => <div>Submit Question Answer</div>}
+              />
+              <Route
+                exact
+                path="/question/poll/:id"
+                render={() => <div>Question Poll View</div>}
+              />
+              <Route
+                exact
+                path="/leaderboard"
+                render={() => <div>Leader Board</div>}
+              />
+              <Route render={() => <h2>404 page not found</h2>} />{' '}
+            </Switch>
+          ) : (
+            <Switch>
+              <Route exact path="/login" render={() => <div>Login</div>} />
+              <Redirect to="/login" />
+            </Switch>
+          )}
         </Fragment>
       </Router>
     )
