d44be3fc8e062769ac08223d63cf186b5b4b4a76
VINOD CHANDRA <vinodchandra.net@gmail.com>
2018-08-13 20:21:07 -0400

    avatar changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ class Header extends Component {
   render() {
     return (
       <div className="header">
-        <p>Would you rather ??</p>
+        <p>Would You Rather ?</p>
       </div>
     )
   }
