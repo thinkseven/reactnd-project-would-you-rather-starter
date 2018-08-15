ee6cd8f4df270db19e78197b8dcd114f32bdaa27
VINOD CHANDRA <vinodchandra.net@gmail.com>
2018-08-15 07:24:17 -0400

    fix add question

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,4 +1,5 @@
 export const GET_USERS = 'GET_USERS'
+export const ADD_QUESTION = 'ADD_QUESTION'
 
 export default function getUsers(users) {
   return {
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,4 @@
-import { GET_USERS } from '../actions/users'
+import { GET_USERS, ADD_QUESTION } from '../actions/users'
 
 export default function users(state = {}, action) {
   switch (action.type) {
@@ -7,6 +7,18 @@ export default function users(state = {}, action) {
         ...state,
         ...action.users,
       }
+    case ADD_QUESTION:
+      return {
+        ...state,
+        [action.question.author]: {
+          ...state[action.question.author],
+          questions: [
+            ...state[action.question.author].questions,
+            action.question.id,
+          ],
+        },
+      }
+
     default:
       return state
   }
